d2b7efbc81aee34bddaab870e6f661ea48e40020
vamsi123-coder <veda2vamsi@gmail.com>
2026-03-15 18:47:56 +0530

    Upload from VS

---
+++ script.js
@@ -0,0 +1,136 @@
+/* Add a todo/ */
+
+let demoarray = [];
+
+function renderTodo(todo) {
+  localStorage.setItem("demoarray", JSON.stringify(demoarray));
+
+  // select unorder list using class
+  const list = document.querySelector(".todo-list");
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  if (todo.deleted) {
+    item.remove();
+    return;
+  }
+
+  const isChecked = todo.checked ? "done" : "";
+  const newlist = document.createElement("li");
+  newlist.setAttribute("class", `todo-item ${isChecked}`);
+  newlist.setAttribute("data-key", todo.id);
+  newlist.innerHTML = `
+<input id="${todo.id}"  type="checkbox"/>
+<label for "${todo.id}"  class="tick js-tick"></label>
+<span>${todo.x}</span>
+<button class="delete-todo js-delete-todo">
+    <button class="delete-todo js-delete-todo">
+        <svg fill="var(--svgcolor)" xmlns="https://www.w3.org/TR/SVG/" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z" />
+        </svg>
+    </button>
+`;
+
+  if (item) {
+    list.replaceChild(newlist, item);
+  } else {
+    list.append(newlist);
+  }
+
+}
+
+// function for adding a todo
+function myFunction(x) {
+  const todoobject = {
+    x,
+    checked: false,
+    id: Date.now(),
+  };
+
+  demoarray.push(todoobject);
+
+  renderTodo(todoobject);
+  console.log(demoarray);
+
+}
+
+function toggleDone(b) {
+  const index = demoarray.findIndex((myitem) => myitem.id === Number(b));
+  demoarray[index].checked = !demoarray[index].checked;
+  renderTodo(demoarray[index]);
+}
+
+function deleteTodo(c) {
+  const index = demoarray.findIndex((myitem) => myitem.id === Number(c));
+  const emptytodo = {
+    deleted: true,
+    ...demoarray[index],
+  };
+  demoarray = demoarray.filter((myitem) => myitem.id !== Number(c));
+  renderTodo(emptytodo);
+}
+
+// select form
+const form = document.querySelector(".formselect");
+
+// add a event listner submit on form
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // select input
+  const input = document.querySelector(".inputselect");
+
+  const text = input.value.trim();
+
+  if (text !== "") {
+    myFunction(text);
+    input.value = "";
+  }
+});
+
+const list = document.querySelector(".js-todo-list");
+list.addEventListener("click", (event) => {
+  if (event.target.classList.contains("js-tick")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  if (event.target.classList.contains("js-delete-todo")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ref = localStorage.getItem("demoarray");
+  if (ref) {
+    demoarray = JSON.parse(ref);
+    demoarray.forEach((t) => {
+      renderTodo(t);
+    });
+  }
+});
+
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
